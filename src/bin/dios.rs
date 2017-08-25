d6d22eca7ee8d8bf24a07743d398801a2850211e
Nicolas Senaud <nicolas@senaud.fr>
2017-08-25 17:58:37 +0200

    Improve CLI utility
    * Rename utility to 'dios'
    * Use clap for argument parsing
    * Add Cargo.lock to ensure CLI tool is reproductible

--- src/bin/dios.rs
+++ src/bin/dios.rs
@@ -1,11 +1,12 @@
 #[macro_use]
+extern crate clap;
+#[macro_use]
 extern crate log;
 extern crate log4rs;
 extern crate libc;
 extern crate wiringpi;
 extern crate home_easy;
 
-use std::env::args;
 use std::time::Duration;
 use std::thread;
 
@@ -14,16 +15,42 @@ use log4rs::append::console::ConsoleAppender;
 use log4rs::config::{Appender, Config, Root};
 
 use home_easy::prelude::{DecodedFrame, EncodedFrame};
-use home_easy::{scheduler_realtime, scheduler_standard, transmit};
+use home_easy::{scheduler_realtime, transmit};
 
 fn main() {
+    // Logger output will be stdout
     let stdout = ConsoleAppender::builder().build();
 
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .build(Root::builder().appender("stdout").build(LogLevelFilter::Info))
-        .unwrap();
+    /* Argument parsing initialization */
+    let matches = clap_app!(myapp =>
+        (name: "dios")
+        (version: "0.1.0")
+        (author: "Nicolas Senaud <nicolas@senaud.fr>")
+        (about: "Switch on and off wireless plugs using GPIO and a 433MHz emetter")
+        (@arg PIN: +required "GPIO pin to use")
+        (@arg SENDER: +required "Sender code, which authentify the emetter to the plug")
+        (@arg INTERRUPTOR: +required "Interruptor number")
+        (@arg STATE: +required "On or off")
+        (@arg verbose:  -v --verbose ... "Sets the level of verbosity")
+    ).get_matches();
 
+    // Define the level of verbosity and configure the logger accordingly
+    let config = match matches.occurrences_of("verbose") {
+        0 => Config::builder()
+                .appender(Appender::builder().build("stdout", Box::new(stdout)))
+                .build(Root::builder().appender("stdout").build(LogLevelFilter::Warn))
+                .unwrap(),
+        1 => Config::builder()
+                .appender(Appender::builder().build("stdout", Box::new(stdout)))
+                .build(Root::builder().appender("stdout").build(LogLevelFilter::Info))
+                .unwrap(),
+        2 | _ => Config::builder()
+                .appender(Appender::builder().build("stdout", Box::new(stdout)))
+                .build(Root::builder().appender("stdout").build(LogLevelFilter::Debug))
+                .unwrap(),
+    };
+
+    // Initilalize the logger.
     log4rs::init_config(config).unwrap();
 
     unsafe {
@@ -33,12 +60,10 @@ fn main() {
         }
     }
 
-    let args: Vec<String> = args().collect();
-
     unsafe { scheduler_realtime() };
 
     let user_input = DecodedFrame {
-        pin: match args[1].parse::<u16>() {
+        pin: match matches.value_of("PIN").unwrap().parse::<u16>() {
             Ok(r) => r,
             Err(e) => {
                 eprintln!("Wrong input for pin parameter!");
@@ -46,7 +71,7 @@ fn main() {
                 return
             },
         },
-        sender: match args[2].parse::<u32>() {
+        sender: match matches.value_of("SENDER").unwrap().parse::<u32>() {
             Ok(r) => r,
             Err(e) => {
                 eprintln!("Wrong input for sender parameter!");
@@ -54,7 +79,7 @@ fn main() {
                 return
             },
         },
-        interruptor: match args[3].parse::<u32>() {
+        interruptor: match matches.value_of("INTERRUPTOR").unwrap().parse::<u32>() {
             Ok(r) => r,
             Err(e) => {
                 eprintln!("Wrong input for interruptor parameter!");
@@ -62,7 +87,7 @@ fn main() {
                 return
             },
         },
-        state: match args[4].parse::<String>() {
+        state: match matches.value_of("STATE").unwrap().parse::<String>() {
             Ok(r) => r,
             Err(e) => {
                 eprintln!("Wrong input for state parameter!");
@@ -89,6 +114,4 @@ fn main() {
         transmit(&output);
         thread::sleep(Duration::from_millis(10));
     }
-
-    unsafe { scheduler_standard() };
 }
